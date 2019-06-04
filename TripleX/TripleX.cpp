a9155e9b2970aa7519baa94c5b6f8ce7b0b1dfae
tubarao25 <felix_dpm@hotmail.com>
2019-06-03 21:36:30 -0300

    Complted TripleX games!

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,9 +1,35 @@
 //Bibliotecas:
 #include <iostream>
+#include <string>
+#include "Functions.h"
+#include <ctime>
+
+
 
 //main
 int main(int argc, char const *argv[]) {
-	
+
+	int PlayerWill = 3, LevelDifficulty = 1;
+	const unsigned int MaxDifficulty = 5;
+	srand(time(NULL));
+
+
+	while (PlayerWill != 0){ // Loop the game until you lose.
+		bool bLevelComplete = PlayGame(&PlayerWill, LevelDifficulty);
+		
+		std::cin.clear();	//clear any errors
+		std::cin.ignore();	//clear any errors
+
+		if (bLevelComplete){
+			++LevelDifficulty;
+		}
+		if (LevelDifficulty > MaxDifficulty)
+		{
+			LevelDifficulty = PrintEndGame();
+			
+		}
+		
+	}
 	
 
 	return 0;
